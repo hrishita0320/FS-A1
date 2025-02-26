a1bf416580ecc92b88da943ba81da407b00cc89b
Hrishita <sugandham.hrishita@gmail.com>
2025-02-27 00:39:55 +0530

    Changes

--- Day12/OCA/string2.java
+++ Day12/OCA/string2.java
@@ -3,9 +3,10 @@ class string2 {
 		String ch = "";
 		for(int i = 0; i< 26; i++){
 			ch = ch + (i+1);
+    }
 		
 		System.out.println(ch);  
-    /*String s1 = "hello";
+    String s1 = "hello";
     String s2 = "Hello";
     String s3 = "HellO".intern();
     String s4 = new String("Hello");
@@ -21,6 +22,6 @@ class string2 {
     }
     if (s2 == s5){
       System.out.println("s1 and s5 are same" );  
-    }*/
+    }
   }
-}
+  }
